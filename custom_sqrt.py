0d7451b4439a9fc79281876571e074f57d3a33fb
WillBAnders <WillBAnders@gmail.com>
2023-01-31 17:58:49 -0500

    push

--- custom_sqrt.py
+++ custom_sqrt.py
@@ -2,6 +2,8 @@ import math
 
 
 def custom_sqrt(number):
+    if math.isnan(number):
+        raise Exception("Invalid number")
     if number < 0:
         return -math.sqrt(-number)
     return math.sqrt(number)
